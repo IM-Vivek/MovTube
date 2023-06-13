1aba0f2fd62a272bbf93704a15a5262338aee7d0
Vivek Kumar Gupta <imvivek94@outlook.com>
2023-06-13 08:34:15 +0530

    Movtube Final

---
+++ Backend/routes/playlist.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express.Router();
+const mongoose = require('mongoose');
+const { User } = require("../models/user");
+
+
+const mongoUri =
+  "mongodb+srv://Admin:testing123@atlascluster.ew7ni5r.mongodb.net/mobtube?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.post("/", (req, res) => {
+
+    User.findOneAndUpdate({ email: req.body.email },{movieData: req.body.movieData}, (err, user) => {
+        if(err){
+            throw (err);
+        }else{
+            User.findOne({ email: req.body.email }, (err, doc) => {
+              if(err){
+                throw err;
+              }else{
+                res.send({
+                  "status": true,
+                  "movieData": doc.movieData
+                })
+              }
+            })
+        }
+      });
+});
+
+module.exports = app;
